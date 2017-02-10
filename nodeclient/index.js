ac313b903a7cf86ba0979ab71e8e37e1f1b2e85e
Ted Wei <jdwei@hotmail.com>
2017-02-10 12:52:22 +0800

    no message

--- nodeclient/index.js
+++ nodeclient/index.js
@@ -2,9 +2,13 @@
 var socket = require('socket.io-client')('http://localhost:3000');
 socket.on('connect', function(){
     console.log("on connect");
+    socket.emit('new message', "Hello World!");
+});
+socket.on('new broadcast data', function(data){
+    console.log("on new broadcast data : " + data);
 });
 socket.on('event', function(data){
-    console.log("on event");
+    console.log("on event : " + data);
 });
 socket.on('disconnect', function(){
     console.log("on disconnect");
--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ var server = require('http').createServer();
 var io = require('socket.io')(server);
 io.on('connection', function(client){
     console.log("on connection : " + client);
+    client.on('new message', function(data){
+        console.log("on new message : " + data);
+        client.broadcast.emit("new broadcast data", data);
+        io.sockets.emit("event", data);
+    });
     client.on('event', function(data){
-        console.log("on event");
+        console.log("on event : " + data);
     });
     client.on('disconnect', function(){
         console.log("on disconnect");
     });
 });
+
 server.listen(3000);
