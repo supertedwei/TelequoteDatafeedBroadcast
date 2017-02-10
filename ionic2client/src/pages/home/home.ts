97f22fa0413a85edc0eb6490a7aa0dfcb8c5ee2f
Ted Wei <jdwei@hotmail.com>
2017-02-10 13:21:49 +0800

    no message

--- ionic2client/src/pages/home/home.ts
+++ ionic2client/src/pages/home/home.ts
@@ -16,9 +16,13 @@ export class HomePage {
     this.socket = io("http://localhost:3000");
     this.socket.on('connect', () => {
       console.log("on connect")
+      this.socket.emit('new message', "Hello World!");
+    });
+    this.socket.on('new broadcast data', function(data){
+        console.log("on new broadcast data : " + data);
     });
     this.socket.on('event', function(data){
-        console.log("on event");
+        console.log("on event : " + data);
     });
     this.socket.on('disconnect', function(){
         console.log("on disconnect");
