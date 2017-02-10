3dea49d9a8c3b4714fcb8f3c4bfa42001ae5fde2
Ted Wei <jdwei@hotmail.com>
2017-02-10 11:11:00 +0800

    “on connect”

--- ionic2client/src/pages/home/home.ts
+++ ionic2client/src/pages/home/home.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import * as io from "socket.io-client";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  socket: SocketIOClient.Socket;
+
   constructor(public navCtrl: NavController) {
-    
+    this.socket = io("http://localhost:3000");
+    this.socket.on('connect', () => {
+      console.log("on connect")
+    });
   }
+  
 
 }
