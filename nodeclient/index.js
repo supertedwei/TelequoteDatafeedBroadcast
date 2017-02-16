068264eb985242c86c3b5bf92c9771b2c9f9a94e
Ted Wei <jdwei@hotmail.com>
2017-02-16 16:15:59 +0800

    implement pushclient

--- nodeclient/index.js
+++ nodeclient/index.js
@@ -1,21 +1,21 @@
+var config = require('./config');
 
-var socket = require('socket.io-client')('http://mobile.img-fx.com:3300');
-// var socket = require('socket.io-client')('http://58.185.12.149:3300');
-//var socket = require('socket.io-client')('http://localhost:3300');
-// var socket = require('socket.io-client')('http://192.168.1.103:3300');
+var socket = require('socket.io-client')(config.pushServer.url);
 socket.on('connect', function(){
     console.log("on connect");
-    socket.emit('new message', "Hello World!");
+    // socket.emit('new message', "Hello World!");
 });
-socket.on('new broadcast data', function(data){
-    console.log("on new broadcast data : " + data);
-});
-socket.on('event', function(data){
-    console.log("on event : " + data);
-});
+// socket.on('new broadcast data', function(data){
+//     console.log("on new broadcast data : " + data);
+// });
+// socket.on('event', function(data){
+//     console.log("on event : " + data);
+// });
 socket.on('counter', function(data){
     console.log("on counter : " + JSON.stringify(data));
 });
 socket.on('disconnect', function(){
     console.log("on disconnect");
 });
+
+console.log("Connecting to push server : " + config.pushServer.url);
