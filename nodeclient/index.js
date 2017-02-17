9b4e534de10f6dfadb0ff1a4dd401604783289b3
Ted Wei <jdwei@hotmail.com>
2017-02-17 10:24:36 +0800

    add socketio-auth

--- nodeclient/index.js
+++ nodeclient/index.js
@@ -3,14 +3,11 @@ var config = require('./config');
 var socket = require('socket.io-client')(config.pushServer.url);
 socket.on('connect', function(){
     console.log("on connect");
-    // socket.emit('new message', "Hello World!");
+    socket.emit('authentication', {username: config.pushServer.username, password: config.pushServer.password});
+    socket.on('authenticated', function() {
+        console.log("authenticated");
+    });
 });
-// socket.on('new broadcast data', function(data){
-//     console.log("on new broadcast data : " + data);
-// });
-// socket.on('event', function(data){
-//     console.log("on event : " + data);
-// });
 socket.on('counter', function(data){
     console.log("on counter : " + JSON.stringify(data));
 });
--- nodeserver/index.js
+++ nodeserver/index.js
@@ -10,6 +10,38 @@ io.on('connection', function(client){
         console.log("on disconnect");
     });
 });
+
+require('socketio-auth')(io, {
+    authenticate: authenticate,
+    postAuthenticate: postAuthenticate,
+    disconnect: disconnect,
+    timeout: 1000
+});
+
+function authenticate(socket, data, callback) {
+    var username = data.username;
+    var password = data.password;
+ 
+//   if (err || !user) {
+//     return callback(new Error("User not found"));
+//   } else {
+    return callback(null, username == password);
+//   };
+}
+
+function postAuthenticate(socket, data) {
+    // var username = data.username;
+    
+    // db.findUser('User', {username:username}, function(err, user) {
+    //     socket.client.user = user;
+    // });
+}
+
+function disconnect(socket) {
+  console.log(socket.id + ' disconnected');
+}
+
+
 pushServer.listen(config.pushServer.port);
 console.log("Push Server started at port : " + config.pushServer.port);
 
