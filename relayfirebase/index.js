356d20331d7aeecff4b82ac4dde65cbde3614710
Ted Wei <jdwei@hotmail.com>
2017-02-27 05:30:03 +0000

    update Firebase quote

--- relayfirebase/index.js
+++ relayfirebase/index.js
@@ -10,6 +10,9 @@ socket.on('connect', function(){
     console.log("on connect");
     socket.on('quote', function(quote) {
         console.log("quote - " + JSON.stringify(quote));
+        var quoteRef = firebase.database().ref("quote");
+        var quoteChildRef = quoteRef.child(quote.symbol);
+        quoteChildRef.set(quote);
     });
     socket.on('disconnect', function(){
         console.log("on disconnect");
